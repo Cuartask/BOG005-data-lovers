b6f3acce347fc6ec79ba46edf8254fdf711f119c
Monik586 <monik.rodriguez586@gmail.com>
2022-08-23 11:13:52 -0500

    cambios

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { example } from './data.js';
+
 import data from './data/athletes/athletes.js';
 
+
 console.log(example, data);
+
 
 console.log(data.athletes);
 console.log(data.athletes[3].name);
