9d16360de95202205155721a7999d5f04fae0026
Katherine Cuartas Cardona <cuartascardonak@gmail.com>
2022-08-31 10:54:25 -0500

    se crean las funciones filtro y calcular

--- src/data.js
+++ src/data.js
@@ -14,40 +14,70 @@ export const dataAtletas = (datos) => {
 }
 
 //ordenarlos en orden alfabetico A-Z
-export const ordenarNombres= (datos) => {
-    return datos.sort( (a, b) =>{
-          if (a.name.toLowerCase() == b.name.toLowerCase()){
-              return 0;
-          }
-          if (a.name < b.name){
-              return -1;
-          }
-            return 1;
-        })    
-          
-  }
-
-  //descendente Z-A
-  export const ordenarNombresZa= (datos) => {
-    return datos.datos.reverse();
-          
-  }
-
-
-//datos = datos.reverse();
-
-
-
-
-
-export const ordenarEdad = (datos) => {
-    datos.sort( (a, b) =>{
-        if (a.age == b.age){
+export const ordenarNombresA_Z = (datos) => {
+    let copyData = [...datos]
+    let order_atletas = copyData.sort((a, b) => {
+        if (a.name.toUpperCase() == b.name.toUpperCase()) {
             return 0;
         }
-        if (a.age < b.age){
+        if (a.name.toUpperCase() < b.name.toUpperCase()) {
             return -1;
         }
         return 1;
-    });
+    })
+    return order_atletas
 }
+
+//descendente Z-A
+export const ordenarNombresZ_A = (datos) => {
+    let copyData = [...datos]
+    copyData.sort((a, b) => {
+        if (a.name.toLowerCase() == b.name.toLowerCase()) {
+            return 0;
+        }
+        if (a.name > b.name) {
+            return -1;
+        }
+        return 1;
+    })
+    return copyData
+}
+
+//filtro por Medallas 
+export function calculo(data, campofiltrado, valor) {
+    const medallas = data.filter(function (datoMedalla) {
+        return datoMedalla[campofiltrado] === valor
+    })
+    return medallas;
+}
+
+
+// ...........CALCULO..........
+
+export function calculo_porcentaje(data, campofiltrado, valor) {
+    const elemento_filtrado = calculo(data, campofiltrado, valor)
+    const porcentaje = (elemento_filtrado.length / data.length) * 100
+    return Math.round(porcentaje);
+}
+
+
+
+
+
+
+
+
+
+//  searchInput.addEventListener("input", () => {
+//     const value = e.target.value.toLowerCase()
+//      pintarData.foreach(dataAtletas  => {
+//          const isVisible =
+//         elemento.name.toLowel,Case().includes(value) ||
+//          elemento.sport.toLowerCase().includes(value)
+//          elemento.element.classList.toggle("hide", !isVisible)
+//         })
+// pintarData(isVisible)
+//  })
+// pintarData(dataAtletas)
+
+
